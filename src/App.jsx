d5fc7f1049b67389f38a340c755cdfc1867bd0de
micael305 <miicael305@gmail.com>
2025-11-25 15:12:43 -0300

    feat: cerrar sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import NotFound from './pages/NotFound.jsx'
+import {Routes, Route, useNavigate} from 'react-router-dom'
+import { useEffect } from "react";
+import './App.css';
+import {supabase} from './supabase/Client.js'
+
 //Vistas de autenticacion
 import Login from './pages/auth/Login.jsx'
 import SignUp from './pages/auth/SignUp.jsx' 
 import Home from './pages/auth/Home.jsx'
-
+//Not Found
+import NotFound from './pages/NotFound.jsx'
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if(!session){
+        navigate('/login');
+        console.log("no hay sesion");
+      }else{
+        navigate('*');
+        console.log("hay sesion");
+      }
+    })
+  }, []);
   return (
     <Routes>
       <Route  path='/' element={<Home/>}/>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
+import Header from "../components/Header/Header";
 function NotFound() {
   return (
     <div style={{color: 'black'}}>
       <h1 >404 - Página no encontrada</h1>
       <p>Lo sentimos, la página que buscas no existe.</p>
+      <Header></Header>
     </div>
   );
 }
